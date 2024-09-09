8dad9fddb7cc67c9d7ac5bf5511e2c57ee7b934a
Michel Vidailhet <michel.vidailhet@gmail.com>
2024-09-09 18:21:26 +0200

    Nettoyage et possibilité de mettre le jeu en pause

--- snake.js
+++ snake.js
@@ -8,66 +8,22 @@ const refreshDelay = 100;
 
 let ctx;
 
-const snakey = new Snake(
-  [
-    [6, 4],
-    [5, 4],
-    [4, 4],
-  ],
-  "right"
-);
-
-var directionKeyCodes = {
-  ArrowLeft: "left",
-  ArrowUp: "up",
-  ArrowRight: "right",
-  ArrowDown: "down",
-};
-
-let isRunning = true;
-
-function createCanvas() {
-  const canvas = document.createElement("canvas");
-  canvas.width = canvasDimensions.width;
-  canvas.height = canvasDimensions.height;
-  canvas.style.border = "1px solid";
-  return canvas;
-}
-
-function appendCanvasToContainer(canvas) {
-  const containerElt = document.getElementById("container");
-  containerElt.appendChild(canvas);
-}
-
-function init() {
-  var canvas = createCanvas();
-  appendCanvasToContainer(canvas);
-  ctx = canvas.getContext("2d");
-  refreshcanvas();
-}
-
-function refreshcanvas() {
-  if (isRunning) {
-    ctx.clearRect(0, 0, canvasDimensions.width, canvasDimensions.height);
-    snakey.advance();
-    snakey.draw();
+class Snake {
+  constructor(body, direction) {
+    this.body = body;
+    this.direction = direction;
   }
-  setTimeout(refreshcanvas, refreshDelay);
-}
 
-function Snake(body, direction) {
-  this.body = body;
-  this.direction = direction;
-  this.draw = function () {
+  draw() {
     ctx.save();
     ctx.fillStyle = "#ff0000";
     for (var i = 0; i < this.body.length; i++) {
       drawblock(ctx, this.body[i]);
     }
     ctx.restore();
-  };
+  }
 
-  this.advance = function () {
+  advance() {
     var nextposition = this.body[0].slice();
     switch (this.direction) {
       case "left":
@@ -87,16 +43,47 @@ function Snake(body, direction) {
     }
     this.body.unshift(nextposition);
     this.body.pop();
-  };
+  }
 
-  this.setdirection = function (newdirection) {
+  setdirection(newdirection) {
     var alloweddirections = Object.values(directionKeyCodes);
     if (!alloweddirections.includes(newdirection)) {
       throw "invalid direction";
     } else {
       this.direction = newdirection;
     }
-  };
+  }
+}
+
+const snakey = new Snake(
+  [
+    [6, 4],
+    [5, 4],
+    [4, 4],
+  ],
+  "right"
+);
+
+var directionKeyCodes = {
+  ArrowLeft: "left",
+  ArrowUp: "up",
+  ArrowRight: "right",
+  ArrowDown: "down",
+};
+
+let isRunning = false;
+
+let overlayElt;
+let overlayTitleElt;
+let overlayStartBtnElt;
+
+function refreshcanvas() {
+  if (isRunning) {
+    ctx.clearRect(0, 0, canvasDimensions.width, canvasDimensions.height);
+    snakey.advance();
+    snakey.draw();
+  }
+  setTimeout(refreshcanvas, refreshDelay);
 }
 
 function drawblock(ctx, position) {
@@ -109,16 +96,49 @@ document.onkeydown = function handlekeydown(e) {
   var keyCodePressed = e.code;
   const allowedKeyCodes = Object.keys(directionKeyCodes);
   if (!allowedKeyCodes.includes(keyCodePressed)) {
-    console.log(keyCodePressed);
-
     if (keyCodePressed === "Space") {
-      isRunning = !isRunning;
+      pauseGame();
     }
-
     return;
   }
   snakey.setdirection(directionKeyCodes[keyCodePressed]);
 };
+
+function startGame() {
+  isRunning = true;
+  overlayElt.classList.add("hidden");
+}
+
+function pauseGame() {
+  isRunning = false;
+  overlayElt.classList.remove("hidden");
+  overlayTitleElt.innerText = "Game Paused";
+  overlayStartBtnElt.innerText = "Resume";
+}
+
+function createCanvas() {
+  const canvas = document.createElement("canvas");
+  canvas.width = canvasDimensions.width;
+  canvas.height = canvasDimensions.height;
+  canvas.style.border = "1px solid";
+  return canvas;
+}
+
+function appendCanvasToContainer(canvas) {
+  const containerElt = document.getElementById("container");
+  containerElt.appendChild(canvas);
+}
+
+function init() {
+  overlayElt = document.getElementById("overlay");
+  overlayTitleElt = document.getElementById("overlay-title");
+  overlayStartBtnElt = document.getElementById("overlay-start-button");
+
+  var canvas = createCanvas();
+  appendCanvasToContainer(canvas);
+  ctx = canvas.getContext("2d");
+  refreshcanvas();
+}
 
 window.onload = function () {
   init();
